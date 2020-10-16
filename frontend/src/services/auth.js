7dc73cc6dcfc688ce47aa7c9a31554138201ddfb
SirSilver <ace.cold.yo@gmail.com>
2020-10-16 15:51:23 +0600

    Removed unused files

--- frontend/src/services/auth.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const AUTH = axios.create({
-    baseURL: process.env.REACT_APP_API_URL
-})
-
-export default {
-    login: credentials => AUTH.post('/auth/login', credentials),
-}
